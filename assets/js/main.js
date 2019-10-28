5fd172b73e539368fecb3ce09bff02656cec882d
Yurii Prytula <yurii.prytula@gmail.com>
2019-10-28 23:04:56 +0200

    add monts/years, formatted code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,37 @@
-(function ($) {
-
+(function($) {
   "use strict";
 
-  $(window).on('load', function () {
-
+  $(window).on("load", function() {
     /* Page Loader active
     ========================================================*/
-    $('#preloader').fadeOut();
-
-
+    $("#preloader").fadeOut();
 
     /* slicknav mobile menu active  */
-    $('.mobile-menu').slicknav({
-      prependTo: '.navbar-header',
-      parentTag: 'liner',
+    $(".mobile-menu").slicknav({
+      prependTo: ".navbar-header",
+      parentTag: "liner",
       allowParentLinks: true,
       duplicate: true,
-      label: '',
+      label: "",
       closedSymbol: '<i class="icon-arrow-right"></i>',
-      openedSymbol: '<i class="icon-arrow-down"></i>',
+      openedSymbol: '<i class="icon-arrow-down"></i>'
     });
 
     /* ==========================================================================
     countdown timer
     ========================================================================== */
-    jQuery('#clock').countdown('2017/10/29', function (event) {
-      var $this = jQuery(this).html(event.strftime('' +
-        '<div class="time-entry days"><span>%-D</span> Days</div> ' +
-        '<div class="time-entry hours"><span>%H</span> Hours</div> ' +
-        '<div class="time-entry minutes"><span>%M</span> Minutes</div> ' +
-        '<div class="time-entry seconds"><span>%S</span> Seconds</div> '));
+    jQuery("#clock").countdown("2017/10/29", function(event) {
+      var $this = jQuery(this).html(
+        event.strftime(
+          "" +
+            '<div class="time-entry days"><span>%-Y</span> Years</div> ' +
+            '<div class="time-entry days"><span>%-m</span> Months</div> ' +
+            '<div class="time-entry days"><span>%-n</span> Days</div> ' +
+            '<div class="time-entry hours"><span>%H</span> Hours</div> ' +
+            '<div class="time-entry minutes"><span>%M</span> Minutes</div> ' +
+            '<div class="time-entry seconds"><span>%S</span> Seconds</div> '
+        )
+      );
     });
-
-
-
   });
-
-}(jQuery));
+})(jQuery);
